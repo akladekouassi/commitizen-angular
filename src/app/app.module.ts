086c41e1a201ce4227660877881903f4c1a8794f
aklade kouassi <akladekouassi@gmail.com>
2020-08-04 14:44:38 +0200

    feat(components): jira/WEB-02 - Add first component feature
    In this PR, i created the first component for the project
    
    BREAKING CHANGE: this PR instead to match, it don't match with the project
    
    feat #02

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstCompComponent } from './components/first-comp/first-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstCompComponent
   ],
   imports: [
     BrowserModule,
