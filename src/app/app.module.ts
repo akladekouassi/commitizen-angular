df4d2d78dd7cb23fa4c5cc9f43bb12a9584e6dda
aklade kouassi <akladekouassi@gmail.com>
2020-08-05 12:24:19 +0200

    fix(app): jira/WEB-01 - setup commitizen
    dsqd qdsds qsdsdq

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FirstCompComponent } from './components/first-comp/first-comp.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FirstCompComponent } from "./components/first-comp/first-comp.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FirstCompComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, FirstCompComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
